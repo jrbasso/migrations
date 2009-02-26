929e8d4678fe26e544ac652db4bbdf35b65fcb0a
Juan Basso <jrbasso@gmail.com>
2009-02-25 22:06:23 -0300

    Created functions for shell script. Revision control is needed.

--- vendors/shells/migration.php
+++ vendors/shells/migration.php
@@ -2,20 +2,138 @@
 
 class MigrationShell extends Shell {
 
+	/*
+	 * Path to installers files
+	 */
+	var $path = null;
+
+	/*
+	 * Name of database
+	 */
+	var $connection = 'default';
+
+	/*
+	 * Number of revision to install/uninstall. -1 if latest
+	 */
+	var $revision = -1;
+
+	/*
+	 * Startup script
+	 */
 	function startup() {
+		if (empty($this->params['path'])) {
+			$this->path = $this->params['working'] . DS . 'installers';
+		} else {
+			$this->path = $this->params['path'];
+		}
+
+		if (!empty($this->params['connection'])) {
+			$this->connection = $this->params['connection'];
+		}
+
+		if (!empty($this->params['revision'])) {
+			$this->revision = $this->params['revision'];
+		}
+
 		parent::startup();
 		$this->out('Migration Shell');
 		$this->hr();
+		$this->out('Path to migrations scripts: ' . $this->path);
+		$this->out('Connection to the database: ' . $this->connection);
+		$this->out('Revision: ' . ($this->revision === -1 ? 'Latest' : $this->revision));
+		$this->hr();
 	}
 
+	/*
+	 * Main
+	 */
 	function main() {
 		$this->help();
 	}
 
-	function install() {}
+	/*
+	 * Install or update database
+	 */
+	function install() {
+		$this->_exec('install');
+	}
 
-	function uninstall() {}
+	/*
+	 * Drop or downgrade database
+	 */
+	function uninstall() {
+		$this->_exec('uninstall');
+	}
 
+	/*
+	 * Check if class and action exists to execute
+	 */
+	function _exec($action) {
+		extract($this->_selectFile()); // Generate $filename and $classname
+		require $filename;
+		if (!class_exists($classname)) {
+			$this->err('The class ' . $classname . ' not in file.');
+			$this->_stop();
+		}
+		$script = new $classname();
+		if (!is_subclass_of($script, 'Migration')) {
+			$this->err('Class ' . $classname . ' not extends Migration.');
+			$this->_stop();
+		}
+		$ok = $script->$action($this->revision, $this->connection);
+		// TODO: Control for revision
+	}
+
+	/*
+	 * Select a file from installer dir
+	 */
+	function _selectFile() {
+		App::import('Core', 'Folder');
+		$folder = new Folder();
+		if (!$folder->cd($this->path)) {
+			$this->err('Specified path not exist.');
+			$this->_stop();
+		}
+		$read = $folder->read();
+		$i = 1;
+		$readFiles = array();
+		foreach ($read[1] as $id => $file) { // Remove files that not is a installer or remove the extension
+			if (strlen($file) < 15 || substr($file, -14) !== '_installer.php') {
+				continue;
+			}
+			$readFiles[$i++] = substr($file, 0, -14);
+		}
+		unset($read);
+		if (empty($readFiles)) {
+			$this->err('Specified path not have installer scripts.');
+			$this->_stop();
+		}
+
+		$readSize = count($readFiles);
+		do {
+			$this->out("\n\nPlease, select a file:");
+			foreach ($readFiles as $id => $file) {
+				$this->out('[' . $id . '] ' . Inflector::humanize($file));
+			}
+			$this->out("[0] Exit. Nothing is changed.\n");
+			$selected = $this->in('Chose: ');
+		} while ($selected < 0 || $selected > $readSize);
+
+		if ($selected == 0) { // Exit option
+			$this->out('Good bye.');
+			$this->_stop();
+		}
+
+		// Return a full path for file and classname
+		return array(
+			'filename' => $this->path . DS . $readFiles[$selected] . '_installer.php',
+			'classname' => Inflector::camelize($readFiles[$selected]) . 'Installer'
+		);
+	}
+
+	/*
+	 * Help
+	 */
 	function help() {
 		$this->out("Usage: cake migration <command> <arg1> <arg2>...");
 		$this->hr();
