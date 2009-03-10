f6cf5d043e15e486292ab6368aa4967bc9f5f7fb
Juan Basso <jrbasso@gmail.com>
2009-03-10 20:48:28 -0300

    need return true to avoid error in shell

--- vendors/shells/templates/single_template.php
+++ vendors/shells/templates/single_template.php
@@ -8,10 +8,12 @@ class :niceName extends AppMigration {
     /* up method */
     function up(){
         // do something!
+		return true;
     }
     /* down method */
     function down(){
         // crash something!
+		return true;
     }
 }
 
